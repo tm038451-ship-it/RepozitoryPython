f48d3bf3cf6752bbb4dd82b3b3730199f0e9454e
tm038451-ship-it <286664214+tm038451-ship-it@users.noreply.github.com>
2026-06-27 20:15:55 +0400

    Delete test_example.py

--- 04_lesson/test_example.py
+++ 04_lesson/test_example.py
@@ -1,10 +1,1 @@
-def test_addition():
-    assert 1 + 1 == 2
-    
- @pytest.mark.parametrize(
-    "input_text, expected_output",
-    [("", " "), ("    ", "    ."), ],
-)
-def test_process_negative(input_text, expected_output):
-    processor = StringProcessor()
-    assert processor.process(input_text) == expected_output   
+
